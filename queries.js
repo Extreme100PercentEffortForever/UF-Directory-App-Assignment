dd31a6e5281711caddd09907ec8cca893f108ba7
benchen88888888 <benchen88888888@gmail.com>
2016-01-20 12:24:24 -0500

    Bens Commit

--- queries.js
+++ queries.js
@@ -1,13 +1,13 @@
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
     config = require('./config');
-	
+
 mongoose.connect(config.db.uri);
 
 var findLibraryWest = function() {
-  /* 
+  /*
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
@@ -18,9 +18,9 @@ var findLibraryWest = function() {
 };
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
    //findOneAndRemove works as well
    Listing.find({ code: 'CABL' }, function(err, entries) {
@@ -28,14 +28,14 @@ var removeCable = function() {
 	  var entry = entries[0];
 	  entry.remove(function(err) {
 		if (err) throw err;
-		console.log('It\'s gone, baby'); 
+		console.log('It\'s gone, baby!  It will never come back!');
 	  });
    });
 };
 var updatePhelpsMemorial = function() {
   /*
-    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
    */
    Listing.find({ code: 'PHL' }, function(err, entries) {
 	   if (err) throw err;
@@ -48,8 +48,8 @@ var updatePhelpsMemorial = function() {
    });
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
    Listing.find({}, function(err, entries) {
 	  var entryMap = {};
