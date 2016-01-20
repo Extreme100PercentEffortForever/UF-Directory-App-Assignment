0edb338409d9b953209a45ea747527187376c622
Tzeryushi <rwhandel@gmail.com>
2016-01-20 12:05:34 -0500

    jtm and queries complete.

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,16 +7,24 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config'),
+	listings = require('./listings.json');
 
 /* Connect to your database */
-
+mongoose.connect(config.db.uri);
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
+ 
+var arr = [];
+for(var x in listings.entries){
+  arr.push(listings.entries[x]);
+}
 
+Listing.create(arr);
 
+console.log('Great Job Space Cadet!');
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
--- queries.js
+++ queries.js
@@ -1,10 +1,20 @@
-/* Fill out these functions using Mongoose queries*/
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config');
+	
+mongoose.connect(config.db.uri);
 
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+	Listing.find({ code: 'LBW' }, function(err, entry) {
+		if (err) throw err;
+		console.log(entry);
+	});
 };
 var removeCable = function() {
   /*
@@ -12,17 +22,42 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+   //findOneAndRemove works as well
+   Listing.find({ code: 'CABL' }, function(err, entries) {
+	  if (err) throw err;
+	  var entry = entries[0];
+	  entry.remove(function(err) {
+		if (err) throw err;
+		console.log('It\'s gone, baby'); 
+	  });
+   });
 };
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+   Listing.find({ code: 'PHL' }, function(err, entries) {
+	   if (err) throw err;
+	   var entry = entries[0];
+	   entry.address = '102 Phelps Lab, Museum Road, Gainesville, FL 32603';
+	   entry.save(function(err) {
+		   if (err) throw err;
+		   console.log(entry);
+	   });
+   });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+   Listing.find({}, function(err, entries) {
+	  var entryMap = {};
+	  entries.forEach(function(entry) {
+		  entryMap[entry._id] = entry;
+	  });
+	  console.log(entryMap);
+   });
 };
 
 findLibraryWest();
